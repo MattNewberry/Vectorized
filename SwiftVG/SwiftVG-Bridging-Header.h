1d465e3f7548406b521b347a65fe7afa5299d949
Brian Christensen <brian@alienorb.com>
2016-04-24 00:00:30 -0500

    Baseline for our fork

--- SwiftVG/SwiftVG-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  SwiftVG-Bridging-Header.h
-//  SwiftVG
-//
-//  Created by Austin Fitzpatrick on 3/19/15.
-//  Copyright (c) 2015 austinfitzpatrick. All rights reserved.
-//
-
-#import "SKUBezierPath+SVG.h"
