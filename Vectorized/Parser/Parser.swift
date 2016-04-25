cbaaf9b92aaec1e2ebcf6e97d91c77cb1b449372
Brian Christensen <brian@alienorb.com>
2016-04-25 13:21:46 -0400

    Experimenting with alternative parser strategy

---
+++ Vectorized/Parser/Parser.swift
@@ -0,0 +1,20 @@
+//
+//  SVGParser.swift
+//  Vectorized
+//
+//  Created by Brian Christensen on 4/25/16.
+//  Copyright © 2016 Alien Orb Software LLC. All rights reserved.
+//
+
+import Foundation
+//import Fuzi
+
+public enum ParserError: ErrorType {
+	
+}
+
+internal class Parser {
+	private init(path: String) throws {
+		
+	}
+}
